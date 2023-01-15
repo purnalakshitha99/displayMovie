f17df8ecf72161c5ecbe7db937dc653a7b183a5d
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-16 01:53:58 +0530

    SFC

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-  state = {};
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onLike}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
-  }
-}
+// class Like extends Component {
+//   state = {};
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onLike}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden="true"
+//       ></i>
+//     );
+//   }
+// }
+
+// export default Like;
+
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onLike}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 
 export default Like;
