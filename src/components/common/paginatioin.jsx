41591fbc80aa54f52790538363ad7f3aea649f1b
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-17 01:15:21 +0530

    1 2 3 page button display

--- src/components/common/paginatioin.jsx
+++ src/components/common/paginatioin.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 
+import _ from "lodash";
 const Plagination = (props) => {
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = itemsCount / pageSize;
+  const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link">1</a>
-        </li>
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
