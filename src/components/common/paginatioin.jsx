3b64ab2239c96b48e4ddbdf60552d5cbb625489f
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-17 00:45:14 +0530

    pagination bootstrap added

---
+++ src/components/common/paginatioin.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const Plagination = (props) => {
+  return (
+    <nav>
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link">1</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Plagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Plagination from "./common/paginatioin";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -24,6 +26,10 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
     console.log("mm", movies[index].liked);
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    console.log("page components");
   };
 
   render() {
@@ -70,6 +76,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Plagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
