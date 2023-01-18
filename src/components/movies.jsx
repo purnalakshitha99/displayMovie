82751b85f762bb69524e012ae07895cfba5d0968
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-18 23:50:01 +0530

    paginate in different pages

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
 import Plagination from "./common/paginatioin";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
@@ -36,9 +37,10 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in the database</p>;
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database.</p>
@@ -54,7 +56,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
